6beb8fbf33fe118ea6fa3bdf8b96efdf0ed02fc2
Vinicius Eidi Honda <vinicius.honda@fatec.sp.gov.br>
2018-12-08 13:19:25 -0200

    Update Variables.cs

--- TMapp/TMapp/Variables.cs
+++ TMapp/TMapp/Variables.cs
@@ -5,7 +5,7 @@ namespace XFGoogleMapSample
     public static class Variables
     {
         // https://developers.google.com/maps/documentation/android-api/signup
-        public const string GOOGLE_MAPS_ANDROID_API_KEY = "AIzaSyDt42XuK3ltzmuy01R0jnj3H3br4e2ieZQ";
+        public const string GOOGLE_MAPS_ANDROID_API_KEY = "api_key";
 
         // https://developers.google.com/maps/documentation/ios-sdk/start#step_4_get_an_api_key
         public const string GOOGLE_MAPS_IOS_API_KEY = "your_google_maps_sdk_for_ios_api_key";
